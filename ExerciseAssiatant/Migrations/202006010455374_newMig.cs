71913404430ac0dc71c66893a8aab13fedf8df25
Erick Arroyo <incspnsoft@gmail.com>
2020-05-31 21:58:38 -0700

    test migrations

---
+++ ExerciseAssiatant/Migrations/202006010455374_newMig.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerciseAssiatant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newMig : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
