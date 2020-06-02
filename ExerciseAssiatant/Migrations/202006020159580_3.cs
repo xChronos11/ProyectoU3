113eda743621ef6e7cf49789b7d52654785511ac
xChronos11 <FranciscoDiaz-xChronos11@users.noreply.github.com>
2020-06-01 19:03:21 -0700

    Vista index, Cambios modelos y controladores

---
+++ ExerciseAssiatant/Migrations/202006020159580_3.cs
@@ -0,0 +1,22 @@
+﻿namespace ExerciseAssiatant.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _3 : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.UserExercises", name: "Admin_Id", newName: "Cliente_Id");
+            RenameIndex(table: "dbo.UserExercises", name: "IX_Admin_Id", newName: "IX_Cliente_Id");
+            AlterColumn("dbo.Exercises", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Exercises", "Name", c => c.String());
+            RenameIndex(table: "dbo.UserExercises", name: "IX_Cliente_Id", newName: "IX_Admin_Id");
+            RenameColumn(table: "dbo.UserExercises", name: "Cliente_Id", newName: "Admin_Id");
+        }
+    }
+}
